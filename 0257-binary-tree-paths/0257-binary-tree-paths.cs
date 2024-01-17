9a503c993197573a4b9efc850036d1b6442afa0d
Yoon Chan ho <cnsj72@gmail.com>
2024-01-17 22:54:16 +0900

    Time: 105 ms (92.31%), Space: 47.4 MB (26.15%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cs
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<string> BinaryTreePaths(TreeNode root) {
+        List<string> paths = new List<string>();
+        string s = "";
+        FindPath(root, s);
+        return paths;
+
+        void FindPath(TreeNode root, string s1){
+            //  현재 노드가 null 이 아닐때
+            if(root != null){
+                //  왼쪽, 오른쪽 자식 노드가 둘 다 없으면
+                if(root.left == null && root.right == null){
+                    //  현재 노드의 값을 문자열에 더하고
+                    s1 = s1 + root.val;
+                    //  리스트에 추가
+                    paths.Add(s1);
+                }else{
+                    //  왼쪽, 오른쪽 자식 노드가 하나라도 있으면
+                    //  현재 노드의 값과 화살표를 문자열에 더하고
+                    //  왼쪽, 오른쪽 자식 노드로 이동
+                    s1 = s1 + root.val + "->";
+                    FindPath(root.left, s1);
+                    FindPath(root.right, s1);
+                }
+            }
+        }
+    }
+
+    
+}
