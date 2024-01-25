010dc99d83952fa5aeed71cdf50db36c68d4d254
Yoon Chan ho <cnsj72@gmail.com>
2024-01-25 22:12:31 +0900

    Time: 16 ms (68.78%), Space: 27.1 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -6,6 +6,9 @@ public:
             char tmp = s[i];
             s[i] = s[n - 1 - i];
             s[n - 1 - i] = tmp;
+
+            //  https://leetcode.com/problems/reverse-string/solutions/3275389/swapping-method-c-omar
+            // swap(s[i], s[n-i-1]);
         }
     }
 };
