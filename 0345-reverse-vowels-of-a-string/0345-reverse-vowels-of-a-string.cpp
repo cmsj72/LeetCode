29ba752e5ce6301f32f5d699f66e4604704449c8
Yoon Chan ho <cnsj72@gmail.com>
2024-01-25 22:58:00 +0900

    Time: 8 ms (49.69%), Space: 10.1 MB (5.08%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -19,5 +19,25 @@ public:
             }
         }
         return ans;
+
+        //  ===================================
+        //  https://leetcode.com/problems/reverse-vowels-of-a-string/solutions/3723102/easy-solution-with-simple-explanation-using-two-pointer
+        // string word = s;
+        // int start = 0;
+        // int end = s.length() - 1;
+        // string vowels = "aeiouAEIOU";
+
+        // while(start < end){
+        //     while(start < end && vowels.find(word[start]) == string::npos){
+        //         start++;
+        //     }
+        //     while(start < end && vowels.find(word[end]) == string::npos){
+        //         end--;
+        //     }
+        //     swap(word[start], word[end]);
+        //     start++;
+        //     end--;
+        // }
+        // return word;
     }
 };
