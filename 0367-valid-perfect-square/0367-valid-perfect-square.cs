253326762f4b9287f778a5cec8a2c47c833f6e2d
Yoon Chan ho <cnsj72@gmail.com>
2024-01-27 14:50:12 +0900

    Time: 31 ms (53.33%), Space: 28.9 MB (8.72%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cs
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cs
@@ -5,5 +5,21 @@ public class Solution {
                 return true;
         }
         return false;
+
+        //  ==========================
+        //  https://leetcode.com/problems/valid-perfect-square/solutions/4617203/binary-search-approach-c
+        // long left = 0;
+        // long right = num;
+        // while(left <= right){
+        //     long mid = left + (right - left) / 2;
+        //     if(mid * mid == num){
+        //         return true;
+        //     }else if(mid*mid < num){
+        //         left = mid + 1;
+        //     }else{
+        //         right = mid - 1;
+        //     }
+        // }
+        // return false;
     }
 }
