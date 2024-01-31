da7353373e3b1238ecd573e9cfde28ed49b3a666
Yoon Chan ho <cnsj72@gmail.com>
2024-01-31 19:30:08 +0900

    Time: 4 ms (47.75%), Space: 8.9 MB (5.12%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -12,5 +12,27 @@ public:
             if(t.second) return t.first;
         }
         return NULL;
+
+        //  =================================
+        //  https://leetcode.com/problems/find-the-difference/solutions/1751380/java-c-python-very-very-easy-to-go-solution
+
+        //  0 ^ a = a;
+        //  a ^ a = 0;
+        // char c = 0;
+        // for(char cs : s) c ^= cs;
+        // for(char ct : t) c ^= ct;
+        // return c;
+
+        //  =================================
+        //  https://leetcode.com/problems/find-the-difference/solutions/1751509/c-time-100-memory-98-6-3-lines-propagate-the-difference-ascii
+        //  아스키코드 합의 차이를 이용한 풀이 방법
+        // for(int i = 0; i < s.length(); i++){
+        //     t[i+1] += t[i] - s[i];
+        // }
+        // return t[t.size() - 1];
+
+        //  =================================
+        //  https://leetcode.com/problems/find-the-difference/solutions/1752248/c-6-approach-from-view-of-sort-count-xor-sum
+        //  기타 다른풀이 같이 있는거
     }
 };
