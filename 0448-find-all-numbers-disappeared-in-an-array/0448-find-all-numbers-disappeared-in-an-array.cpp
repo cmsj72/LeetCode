165e776569b27d334f4bc08cd2ed1edfdb6c1b1a
Yoon Chan ho <cnsj72@gmail.com>
2024-02-16 21:33:24 +0900

    Time: 63 ms (43.77%), Space: 37.2 MB (41.33%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cpp
@@ -21,5 +21,47 @@ public:
             ans.push_back(cnt++);
         }
         return ans;
+        //  ==============================  //
+        //  https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/solutions/1583736/c-python-all-6-solutions-w-explanations-binary-search-hashset-2x-o-1-space-approach
+        //  1.Brute-Force
+        // vector<int> ans;
+        // for(int i = 1; i <=size(nums); i++){
+        //     if(find(begin(nums), end(nums), i) == end(nums)){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        //  2.Sort & Binary-Search
+        //  내장 함수? 가 있네
+        // sort(begin(nums), end(nums));
+        // vector<int> ans;
+        // for(int i = 1; i <= size(nums); i++){
+        //     if(!binary_search(begin(nums), end(nums), i)){
+        //         ans.push_back(i);
+        //     }
+        // }
+        // return ans;
+
+        //  3.HashSet
+        // unordered_set<int> s(begin(nums), end(nums));
+        // vector<int> ans(size(nums) - size(s));
+        // for(int i = 1, j = 0; i <= size(nums); i++){
+        //     if(!s.count(i))
+        //         ans[j++] = i;
+        // }
+        // return ans;
+
+        //  4.Boolean array
+        // vector<bool> seen(size(nums)+1);
+        // vector<int> ans;
+        // for(auto c : nums)
+        //     seen[c] = true;
+        // for(int i = 1; i <= size(nums); i++){
+        //     if(!seen[c]) ans.push_back(i);
+        // }
+        // return ans;
+
+        //  5. 이거는 길어서 링크 참조
     }
 };
