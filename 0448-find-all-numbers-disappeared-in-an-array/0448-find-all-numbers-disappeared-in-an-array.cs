7657bb21f5427e933fbac4e44a4c478fa55c8309
Yoon Chan ho <cnsj72@gmail.com>
2024-02-16 21:56:51 +0900

    Time: 192 ms (91.28%), Space: 65 MB (18.19%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cs
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public IList<int> FindDisappearedNumbers(int[] nums) {
+        //  c++에서 본 HashSet을 이용한 방법
         HashSet<int> hs = nums.ToHashSet();
         List<int> list = new(nums.Length - hs.Count);
         for(int i = 1, j = 0; i <= nums.Length; i++){
@@ -9,5 +10,20 @@ public class Solution {
         }
         return list;
         
+        //  ==============================  //
+        //  https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/solutions/545609/c-using-o-1-space-o-n-runtime
+        for(int i = 0; i < nums.Length; i++){
+            int index = Math.Abs(nums[i]) - 1;
+            if(nums[index] > 0){
+                nums[index] = -nums[index];
+            }            
+        }
+        List<int> result = new List<int>();
+            for(int i = 0; i < nums.Length; i++){
+                if(nums[i] > 0){
+                    result.Add(i+1);
+                }
+            }
+            return result;
     }
 }
