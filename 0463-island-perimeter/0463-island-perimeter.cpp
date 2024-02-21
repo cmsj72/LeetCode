6c70d44cdbdf52dd178ea8a23a21ba4a7c9fc701
Yoon Chan ho <cnsj72@gmail.com>
2024-02-21 23:29:47 +0900

    Time: 73 ms (74.08%), Space: 104.7 MB (8.37%) - LeetHub

---
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+int dr[4] = {-1,1,0,0};
+int dc[4] = {0,0,-1,1};
+bool visited[100][100];
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans = 0;
+        int rSize = grid.size();
+        int cSize = grid[0].size();
+        int curRow,curCol;
+        for(int r = 0; r < rSize; r++){
+            for(int c = 0; c < cSize; c++){
+                if(grid[r][c] == 1){
+                    curRow = r;
+                    curCol = c;
+                    break;
+                }
+            }
+            if(curRow != -1) break;
+        }
+        queue<pair<int,int>> que;
+        que.push({curRow, curCol});
+        visited[curRow][curCol] = true;
+        while(!que.empty()){
+            curRow = que.front().first;
+            curCol = que.front().second;
+            que.pop();
+            for(int i = 0; i < 4; i++){
+                int nextRow = curRow + dr[i];
+                int nextCol = curCol + dc[i];
+                if(nextRow >= 0 && nextRow < rSize && nextCol >= 0 && nextCol < cSize){
+                    if(grid[nextRow][nextCol] == 1 && !visited[nextRow][nextCol]){
+                        que.push({nextRow,nextCol});
+                        visited[nextRow][nextCol] = true;
+                    }else if(grid[nextRow][nextCol] == 0){
+                        ans++;
+                    }
+                }else{
+                    ans++;
+                }         
+            }
+        }
+        return ans;
+    }
+    //  [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+};
