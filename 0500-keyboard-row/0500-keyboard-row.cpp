a5d7519773d3419572fbe3afbab3457383262c45
Yoon Chan ho <cnsj72@gmail.com>
2024-02-28 22:04:44 +0900

    Time: 4 ms (11.38%), Space: 8 MB (55.72%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -18,5 +18,33 @@ public:
             }
         }
         return ans;
+
+        //  ==============================  //
+        //  https://leetcode.com/problems/keyboard-row/solutions/373966/c-easy-solution-faster-than-100-with-explaination
+        // unordered_set<char> set1{'q','w','e','r','t','y','u','i','o','p'};
+        // unordered_set<char> set2{'a','s','d','f','g','h','j','k','l'};
+        // unordered_set<char> set3{'z','x','c','v','b','n','m'};
+        // for(string word : words){
+        //     bool flag = true;
+        //     unordered_set<char> setTemp;
+
+        //     if(set1.find(tolower(word[0])) != set1.end()){
+        //         setTemp = set1;
+        //     }else if(set2.find(tolower(word[0])) != set2.end()){
+        //         setTemp = set2;
+        //     }else{
+        //         setTemp = set3;
+        //     }
+
+        //     for(char ch : word){
+        //         if(setTemp.find(tolower(ch)) == setTemp.end()){
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag)
+        //         res.push_back(word);
+        // }
+        // return res;
     }
 };
