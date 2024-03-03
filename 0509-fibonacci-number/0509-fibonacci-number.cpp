762097bd7c903cf9df1637b6d5075a8029e99f98
Yoon Chan ho <cnsj72@gmail.com>
2024-03-03 21:01:04 +0900

    Time: 2 ms (78.44%), Space: 7 MB (50.14%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -8,5 +8,22 @@ public:
             arr[i] = arr[i - 1] + arr[i - 2];
         }
         return arr[n];
+        
+        //  ==============================  //
+        //  https://leetcode.com/problems/fibonacci-number/solutions/3013873/dynamic-programming-memoization-tabulation-space-optimization-techniques
+        //  Using Resursion
+        // if(n <= 1) return n;
+        // return fib(n-1)+fib(n-2);
+
+        //  Iterative Solution / Space Optimized Soln.
+        if(n <= 1) return n;
+        int prev2 = 0;
+        int prev = 1;
+        for(int i = 2; i <= n; i++){
+            int curr_i = prev2 + prev;
+            prev2 = prev2;
+            prev = curr_i;
+        }
+
     }
 };
