8651127f41ea944d696e94cd78036232dee41ce7
Yoon Chan ho <cnsj72@gmail.com>
2024-04-18 00:08:15 +0900

    Time: 4 ms (48.24%), Space: 10.2 MB (71.13%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.cpp
+++ 0819-most-common-word/0819-most-common-word.cpp
@@ -27,5 +27,22 @@ public:
             }
         }
         return tmp;
+
+        //  ==============================  //
+        //  submission 제일 빠른코드
+        // unordered_set<string> ban(banned.begin(), banned.end());
+        // unordered_map<string, int> count;
+        // for(auto &c: p) c = isalpha(c) ? tolower(c) : ' ';
+        // istringstream iss(p);
+        // string w;
+        // pair<string, int> res("", 0);
+        // //  w의 자료형에 맞는 정보가 없을 때까지
+        // //  계속 스트림에서 w로 자료를 추출/복사. 끝에 도달하면 끝난다.
+        // //  https://doitnow-man.tistory.com/entry/C-%EA%B0%9C%EB%B0%9C%EC%9E%90%EB%90%98%EA%B8%B0-6-istringstream-ostringstream-stringstream-%EC%82%AC%EC%9A%A9%EB%B2%95
+        // while(iss >> w){
+        //     if(band.find(w) == ban.end() && ++count[w] > res.second)
+        //         res = make_pair(w, count[w]);
+        // }
+        // return res.first;
     }
 };
