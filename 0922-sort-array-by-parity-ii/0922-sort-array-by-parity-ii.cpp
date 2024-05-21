d691813790de19826752b12c184fe33bfbc2243b
Yoon Chan ho <cnsj72@gmail.com>
2024-05-21 23:36:24 +0900

    Time: 1804 ms (5.08%), Space: 23.8 MB (77.71%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.cpp
@@ -11,5 +11,22 @@ public:
             }
         }
         return nums;
+
+        //  ==============================  //
+        //  https://leetcode.com/problems/sort-array-by-parity-ii/solutions/1490847/c-two-pointers-solution
+        // int n = nums.size();
+        // int i = 0, j = 1;
+        // while(i < n && j < n){
+        //     if(nums[i] % 2 == 0){
+        //         i += 2;
+        //     }
+        //     else if(nums[j] % 2 == 1){
+        //         j += 2;
+        //     }
+        //     else{
+        //         swap(nums[i], nums[j]);
+        //     }
+        // }
+        // return nums;
     }
 };
